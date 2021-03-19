a378b58e19ec34ab325dd43cb1102ecb0852918c
kagimotoeiji <thank.eiji1017@gmail.com>
2021-03-19 20:08:00 +0900

    hello_world_sample

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User
+  def initialize
+    @first_name = "Eiji"
+    @last_name = "Kagimoto"
+    @birthday = "2000/10/17"
+    @age = 20
+    @birthplace = "Nara"
+    @hobby = "Game"
+  end
+  def introduce
+    <<~EOS
+    私の名前は#{@first_name + @last_name}です。
+    誕生日は#{@birthday}で、年齢は#{@age}歳。
+    出身地は#{@birthplace}で、趣味は#{@hobby}です。
+    EOS
+  end
+end
